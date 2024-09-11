aa88fd190666d41b3107316cba5e226b51bd4492
lee1jaden_MSIModern <jadenlee1@att.net>
2024-09-11 14:42:40 -0600

    Retrieve data to make plot of pitch arsenals.

--- trackman/trackman.py
+++ trackman/trackman.py
@@ -1,15 +1,32 @@
 import csv
 
+pitchers = {}
+pitchFields = { "Name": "Pitcher",
+                "ThrowingHand": "PitcherThrows",
+                "Set": "PitcherSet",
+                "PitchType": "AutoPitchType",
+                "Speed": "RelSpeed",
+                "VerticalBreak": "InducedVertBreak",
+                "HorizontalBreak": "HorzBreak"
+                }
+
 # Read and write a TrackMan CSV file.
 with open('./trackman/data/example-trackman.csv', newline='') as readFile:
     reader = csv.DictReader(readFile)
-    with open('./trackman/data/filtered-data.csv', 'w', newline='') as writeFile:
-        fieldnames = ["Pitcher", "Batter"]
-        writer = csv.DictWriter(writeFile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for row in reader:
-            entry = {}
-            for field in fieldnames:
-                entry.update({field: row[field]})
-            writer.writerow(entry)
+    for row in reader:
+        if not row["PitcherId"] in pitchers:
+            pitchers.update({row["PitcherId"]: {"Name": row[pitchFields["Name"]], 
+                                                "ThrowingHand": row[pitchFields["ThrowingHand"]], 
+                                                "Pitches": []
+                                                }})
+        pitchers[row["PitcherId"]]["Pitches"].append({"Set": row[pitchFields["Set"]], 
+                                                "PitchType": row[pitchFields["PitchType"]], 
+                                                "Speed": row[pitchFields["Speed"]], 
+                                                "VerticalBreak": row[pitchFields["VerticalBreak"]], 
+                                                "HorizontalBreak": row[pitchFields["HorizontalBreak"]], 
+                                                })
+            
+for pitcher in pitchers.values():
+    # Make matplotlib chart of pitch profiles
+    template = "{} threw {} pitches."
+    print(template.format(pitcher["Name"], len(pitcher["Pitches"])))
